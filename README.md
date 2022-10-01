34f0b1b9be207e6d518df1e222390fa15edb2b17
hyunjin <leehj0110@kakao.com>
2022-10-01 14:55:53 +0900

    chore: add deploy branch

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # How to MOZI
 
+MOZI를 개발하면서 고민한 내용을 적은 블로그와 MOZI의 기능에 대해 설명한 내용을 포함한 문서입니다.
+
 ### Installation
 
 ```
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula")
 const config = {
   title: "MOZI",
   tagline: "MOZI의 개발기록",
-  url: "https://your-docusaurus-test-site.com",
+  url: "https://github.com/team-yaza",
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
@@ -18,7 +18,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "team-yaza", // Usually your GitHub org/user name.
   projectName: "MOZI", // Usually your repo name.
-
+  deploymentBranch: "gh-pages",
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
@@ -115,7 +115,7 @@ const config = {
               },
               {
                 label: "GitHub",
-                href: "https://github.com/facebook/docusaurus",
+                href: "https://github.com/team-yaza",
               },
             ],
           },
